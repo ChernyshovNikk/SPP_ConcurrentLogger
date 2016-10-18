596a85e5ec5ce7d3194f1603a5afb98c315a7e76
Nikita Chernyshov <NikkOrange96@gmail.com>
2016-10-18 15:16:31 +0300

    Make condition things
    Make condition things

---
+++ ConcurrentLogger/ConcurrentLogger/Logger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcurrentLogger
+{
+    public class Logger : ILogger
+    {
+        public Logger(int bufferLimit, ILoggerTarget[] targets)
+        {
+            //...
+        }
+        //...
+    }
+}
---
+++ ConcurrentLogger/ConcurrentLogger/LoggerInformation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcurrentLogger
+{
+    public class LoggerInformation
+    {
+        public LogLevel loggerLevel;
+        public string loggerMessage;
+        public string loggerDateAndTime;
+
+        public LoggerInformation(LogLevel level, string message)
+        {
+            this.loggerLevel = level;
+            this.loggerMessage = message;
+            this.loggerDateAndTime = DateTime.Now.ToString();
+        }
+
+        public string GetLoggerStringMessage()
+        {
+            return String.Format("[{0}] {1} {2}\r\n", loggerDateAndTime, loggerLevel, loggerMessage);
+        }
+    }
+}
