42355a90a7b0a1613a018ab7fd569f58162d4e44
Nikita Chernyshov <NikkOrange96@gmail.com>
2016-10-18 15:14:56 +0300

    Start
    Start

---
+++ ConcurrentLogger/UnitTest_Logger/Logger_Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest_Logger
+{
+    [TestClass]
+    public class Logger_Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
